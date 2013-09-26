bd0f81e8dcb9395522f0a27958b8b364076ef8e8
Aaron <aaron.velthoven@gmail.com>
2013-09-26 08:20:55 -0400

    Finish user edit, update, index, and destroy actions

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -30,7 +30,9 @@ describe "AuthenticationPages" do
       before { sign_in user }
       
       it { should have_title(user.name) }
+      it { should have_link('Users', href: users_path) }
       it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
       
@@ -42,6 +44,19 @@ describe "AuthenticationPages" do
   end
   
   describe "authorization" do
+    
+    describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
+      
+      before { sign_in non_admin, no_capybara: true }
+      
+      describe "submitting a DELETE request to the USers#destroy action" do
+        before { delete user_path(user) }
+        specify { expect(response).to redirect_to(root_url) }
+        
+      end
+    end
     
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
@@ -65,12 +80,18 @@ describe "AuthenticationPages" do
       describe "in the Users controller" do
         before { visit edit_user_path(user) }
         it { should have_title('Sign in') }
+      
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_title('Sign in') }
+        end
       end
       
       describe "submitting to the update action" do
         before { patch user_path(user) }
         specify { expect(response).to redirect_to(signin_path) }
       end
+      
     end
     
     describe "as wrong user" do
